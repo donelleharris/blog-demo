eafcd6912667566aa257d99372c13e649143e6bf
Donelle Harris <donelle.harris@gmail.com>
2020-11-24 09:27:09 -0600

    first commit

---
+++ src/test/java/com/donelle_harris/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donelle_harris.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
