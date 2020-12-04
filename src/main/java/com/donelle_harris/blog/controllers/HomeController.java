0dd99d53d1aeacf4fbf445d8be675345655c1955
Donelle Harris <donelle.harris@gmail.com>
2020-12-03 22:45:56 -0600

    Add most recent post to landing page

--- src/main/java/com/donelle_harris/blog/controllers/HomeController.java
+++ src/main/java/com/donelle_harris/blog/controllers/HomeController.java
@@ -1,18 +1,43 @@
 package com.donelle_harris.blog.controllers;
 
+import com.donelle_harris.blog.models.Post;
+import com.donelle_harris.blog.repositories.PostRepository;
+import com.donelle_harris.blog.repositories.TagRepository;
+import com.donelle_harris.blog.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
+    private final PostRepository postDao;
+    private final UserRepository userDao;
+    private final TagRepository tagDao;
+
+    public HomeController(PostRepository postDao, UserRepository userDao, TagRepository tagDao){
+        this.postDao = postDao;
+        this.userDao = userDao;
+        this.tagDao = tagDao;
+    }
+
     @GetMapping("/")
-    public String index() {
-        return "/posts/index";
+    public String index(Model model) {
+        List postList = postDao.findAll();
+        int lastPostIndex = postList.size()-1;
+        Post lastPost = (Post) postList.get(lastPostIndex);
+        model.addAttribute("post", lastPost);
+        return "/home";
     }
 
     @GetMapping("/home")
-    public String welcome(){
-        return "/posts/index";
+    public String welcome(Model model){
+        List postList = postDao.findAll();
+        int lastPostIndex = postList.size()-1;
+        Post lastPost = (Post) postList.get(lastPostIndex);
+        model.addAttribute("post", lastPost);
+        return "/home";
     }
 }
